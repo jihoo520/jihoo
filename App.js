25cdca6b0bf785fcca1458c60cfb64c1ec2a34b5
jihoo520 <king107031@naver.com>
2025-04-17 18:21:06 +0900

    0417 react 공부

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
 import Todo from './Todo';
-import { Example } from './Example';
+import {List, Paper} from '@mui/material'
 
 function App() {
+//하나의 할 일을 객체로 관리할 것이다
+//id, title, done
+const [items, setItem] = useState([
+  {
+  id:"0",
+  title:"Hello world 1",
+  done: true
+},
+{
+  id:"1",
+  title:"Hello world 2",
+  done: false
+}])
+
+//react는 key속성에 들어있는 값을 참고해서, 리스트의 요소가 변경될 경우
+//어떤 요소가 변경되었는지 빠르게 파악할 수 있다.
+const todoItems = items.length > 0 && 
+//Paper컴포넌트
+//종이 같은 표면 효과를 제공하는 컨테이너 컴포넌트
+//elecation(그림자깊이)를 통해 높낮이를 표현하고
+//배경색과 그림자 효과로 콘텐츠를 돋보이게 한다.
+<Paper style={{margin: 16}}>
+<List> {/* 일련의 항목을 세로로 나열하는 컨테이너 역할 */}
+          {items.map((item) => (
+            <Todo item={item} key={item.id} />
+          ))}
+        </List>
+</Paper>
+//items.map(item => <Todo item={item} key={item.id} />)
+
 
   return (
     <div className="App">
-      <Example />
+      {todoItems}
     </div>
   );
 }
--- Todo.js
+++ Todo.js
@@ -1,19 +1,35 @@
+import {useState} from 'react'
+import {ListItem, 
+        ListItemText, 
+        InputBase, 
+        Checkbox} from '@mui/material'
+
 //현재 파일에서는 checkBox와 label 컴포넌트를 만들어보자
 
+let Todo = (props) => {
 
-let Todo = () => {
+    const [item, setItem] = useState(props.item);
     return(
-        //html 코드가 들어가는 부분
-        //속성을 쓸떄 카멜케이스로 작성하기
+
+        //html코드가 들어가는 부분
+        //속성을 쓸 때 카멜케이스로 작성하기
         //onclick -> onClick
         //class -> className
-        <div className="Todo">
-            <input type="checkbox" id="todo0" name="todo0" value="todo0"/>
-            {/* label 태그는 for속성에 name값으로 연결해서 어떤 요소와 연결될지 지정 */}
-            <label for="todo0">Todo 컴포넌트 만들기</label>
-        </div>
+        <ListItem>
+            <Checkbox checked={item.done}/>
+            <ListItemText>
+                <InputBase
+                    inputProps={{"aria-label" : "naked"}}
+                    type="text"
+                    id={item.id}
+                    name={item.id}
+                    value={item.title}
+                    multiline={true}
+                    fullWidth={true}
+                />
+            </ListItemText>
+        </ListItem>
     )
-
 }
 
 export default Todo;
